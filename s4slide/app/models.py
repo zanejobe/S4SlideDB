c2b91623341832de5f64cb3de127e05dbef032f5
BryannaGaede <bpriddy@mymail.mines.edu>
2020-05-18 13:08:09 -0600

    created model summary_info_id

--- s4slide/app/models.py
+++ s4slide/app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+#**********************************************************************
+#* SUMMARY_INFO_ID
+#* 
+#* CONFUSED ABOUT THE DATA TYPE FOR THE FOLLOWING VARIABLES: 
+#*      - Parent ID
+#*      - Object Type 
+#**********************************************************************/
+class summary_info_id (models.Model)
+    #id = models.AutoField(primary_key=True), - already automatic
+    pid = models.IntegerField()
+    name = models.CharField(max_length = 50)
+    aliases = models.CharField(max_length = 50) 
+    frontal_confinement = models.BooleanField()
+    object_type BIT(1)
+    ss_depth_m = models.FloatField()
+    ss_time_twtt = models.FloatField()
+    ss_depth_notes = modles.TextField()
+    def __str__(self)
+        return self.name
