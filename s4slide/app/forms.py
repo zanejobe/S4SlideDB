7f09850e7cf45992fb27b8f722ada4ea8dbca964
Vlad Muresan <vmuresan98@yahoo.com.com>
2020-05-18 18:57:08 -0600

    Now we can receive input from search bar and output it to see that we have received it

---
+++ s4slide/app/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class databaseSearch(forms.Form):
+    search = forms.CharField()
--- s4slide/app/views.py
+++ s4slide/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import databaseSearch
 
 def index(request):
     return render(request, "index.html")
@@ -16,4 +17,15 @@ def people(request):
     return render(request, "people.html")
 
 def viewer(request):
-    return render(request, "viewer.html")
+    #attempt to read in information from search bar
+
+    if request.method == 'POST':
+        form = databaseSearch(request.POST)
+        if form.is_valid():
+            inputTerm = form.cleaned_data['search']
+
+            print(inputTerm)
+
+    form = databaseSearch()
+
+    return render(request, "viewer.html", {'form': form})
