136d06c7baf4c0f76b8180a458811e628cea7381
Vlad Muresan <vmuresan98@yahoo.com.com>
2020-05-19 12:35:57 -0600

    Added more checkboxes to the form class.

--- s4slide/app/forms.py
+++ s4slide/app/forms.py
@@ -2,3 +2,9 @@ from django import forms
 
 class databaseSearch(forms.Form):
     search = forms.CharField()
+    loc = forms.CheckboxInput()
+    cont = forms.CheckboxInput()
+    date = forms.CheckboxInput()
+    strat = forms.CheckboxInput()
+    age = forms.CheckboxInput()
+    loc2 = forms.CheckboxInput()
--- s4slide/app/views.py
+++ s4slide/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from forms import databaseSearch
 
 def index(request):
 	return render(request, "index.html")
