963e09c3dfbd78713080d977dfa3dd81aae5f060
Lucas Kitaev <lkitaev@mymail.mines.edu>
2020-06-06 17:04:41 -0600

    Allow blank fields in admin form, add unverify action

--- s4slide/app/admin.py
+++ s4slide/app/admin.py
@@ -3,13 +3,17 @@ from .models import *
 
 def verify(modeladmin, request, queryset):
 	queryset.update(verified=True)
-verify.short_description = "Mark selected rows as verified"
+verify.short_description = "Verify selected rows"
+
+def unverify(modeladmin, request, queryset):
+	queryset.update(verified=False)
+unverify.short_description = "Unverify selected rows"
 
 class MetaAdmin(admin.ModelAdmin):
 	date_hierarchy = "upload_date"
 	list_display = ("landslide", "data_type", "data_source", "contact_name", "contact_email")
 	list_filter = ("verified",)
-	actions = [verify]
+	actions = [verify, unverify]
 
 	def delete_queryset(self, request, queryset):
 		summary_info_id.objects.filter(id__in=queryset).delete()
