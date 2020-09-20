4082660ddb8a31ede4e45daa26a588e766bc5474
Lucas Kitaev <lkitaev@mymail.mines.edu>
2020-09-19 22:34:33 -0600

    Add model validation

--- s4slide/app/models.py
+++ s4slide/app/models.py
@@ -1,20 +1,32 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class summary_info_id(models.Model):
-	pid = models.IntegerField(null=True)
-	name = models.CharField(max_length=100)
-	alias = models.CharField(max_length=100)
-	frontal_confinement = models.BooleanField(null=True)
+	# TODO parent is currently unused
+	parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True);
+	name = models.CharField(max_length=100, blank=True)
+	alias = models.CharField(max_length=100, blank=True)
+	frontal_confinement = models.BooleanField(null=True, blank=True)
 	TYPE_CHOICES = [("S", "Single"), ("M", "Multiple")]
-	object_type = models.CharField(max_length=1, choices=TYPE_CHOICES) # TODO check that pid is not null if multiple
-	ss_depth_m = models.FloatField(null=True)
-	ss_time_twtt = models.FloatField(null=True)
-	ss_depth_notes = models.TextField()
-	comments = models.TextField()
-	category = models.CharField(max_length=100)
+	object_type = models.CharField(max_length=1, choices=TYPE_CHOICES, blank=True)
+	ss_depth_m = models.FloatField(null=True, blank=True)
+	ss_time_twtt = models.FloatField(null=True, blank=True)
+	ss_depth_notes = models.TextField(blank=True)
+	comments = models.TextField(blank=True)
+	category = models.CharField(max_length=100, blank=True)
 
 	def __str__(self):
-		return self.name + " (" + self.alias + ")"
+		return self.name
+
+	def clean(self):
+		references = summary_info_id.objects.filter(parent__exact=self.id).count()
+		if self.object_type == "S" and references > 0:
+			raise ValidationError(_("Single objects must not be referenced as a parent."))
+		if self.object_type == "M":
+			if self.parent is not None:
+				raise ValidationError(_("Multiple objects may not reference a parent."))
+			if references < 1:
+				raise ValidationError(_("Multiple objects must have one or more references as a parent."))
 
 	class Meta:
 		ordering = ["id"]
@@ -25,33 +37,33 @@ class landslide_morphometrics(summary_info_id):
 			parent_link=True, primary_key=True)
 	latitude = models.FloatField()
 	longitude = models.FloatField()
-	w_depth_min = models.FloatField(null=True)
-	w_depth_max = models.FloatField(null=True)
-	w_depth_notes = models.TextField()
-	lt = models.FloatField(null=True)
-	ld = models.FloatField(null=True)
-	le = models.FloatField(null=True)
-	l_notes = models.TextField()
-	ls = models.FloatField(null=True)
-	hs = models.FloatField(null=True)
-	he = models.FloatField(null=True)
-	ws = models.FloatField(null=True)
-	scarp_surf_nat = models.TextField()
-	scarp_notes = models.TextField()
-	wd = models.FloatField(null=True)
-	td_max_m = models.FloatField(null=True)
-	td_max_twtt = models.FloatField(null=True)
-	tu_max_m = models.FloatField(null=True)
-	tu_max_twtt = models.FloatField(null=True)
-	t_notes = models.TextField()
-	dep_notes = models.TextField()
-	ht = models.FloatField(null=True)
-	s = models.FloatField(null=True)
-	s_notes = models.TextField()
-	ss = models.FloatField(null=True)
-	ss_notes = models.TextField()
-	st = models.FloatField(null=True)
-	st_notes = models.TextField()
+	w_depth_min = models.FloatField(null=True, blank=True)
+	w_depth_max = models.FloatField(null=True, blank=True)
+	w_depth_notes = models.TextField(blank=True)
+	lt = models.FloatField(null=True, blank=True)
+	ld = models.FloatField(null=True, blank=True)
+	le = models.FloatField(null=True, blank=True)
+	l_notes = models.TextField(blank=True)
+	ls = models.FloatField(null=True, blank=True)
+	hs = models.FloatField(null=True, blank=True)
+	he = models.FloatField(null=True, blank=True)
+	ws = models.FloatField(null=True, blank=True)
+	scarp_surf_nat = models.TextField(blank=True)
+	scarp_notes = models.TextField(blank=True)
+	wd = models.FloatField(null=True, blank=True)
+	td_max_m = models.FloatField(null=True, blank=True)
+	td_max_twtt = models.FloatField(null=True, blank=True)
+	tu_max_m = models.FloatField(null=True, blank=True)
+	tu_max_twtt = models.FloatField(null=True, blank=True)
+	t_notes = models.TextField(blank=True)
+	dep_notes = models.TextField(blank=True)
+	ht = models.FloatField(null=True, blank=True)
+	s = models.FloatField(null=True, blank=True)
+	s_notes = models.TextField(blank=True)
+	ss = models.FloatField(null=True, blank=True)
+	ss_notes = models.TextField(blank=True)
+	st = models.FloatField(null=True, blank=True)
+	st_notes = models.TextField(blank=True)
 
 	class Meta:
 		constraints = [
@@ -63,17 +75,17 @@ class landslide_metrics(summary_info_id):
 	landslide = models.OneToOneField(
 			summary_info_id, on_delete=models.CASCADE,
 			parent_link=True, primary_key=True)
-	attachment = models.BooleanField(null=True)
-	surf_basal = models.CharField(max_length=25)
-	surf_upper = models.TextField(max_length=25)
-	a = models.FloatField(null=True)
-	a_notes = models.TextField()
-	v = models.FloatField(null=True)
-	v_notes = models.TextField()
-	age = models.CharField(max_length=50)
-	age_error = models.CharField(max_length=50)
-	age_notes = models.TextField()
-	features = models.TextField()
+	attachment = models.BooleanField(null=True, blank=True)
+	surf_basal = models.CharField(max_length=25, blank=True)
+	surf_upper = models.TextField(max_length=25, blank=True)
+	a = models.FloatField(null=True, blank=True)
+	a_notes = models.TextField(blank=True)
+	v = models.FloatField(null=True, blank=True)
+	v_notes = models.TextField(blank=True)
+	age = models.CharField(max_length=50, blank=True)
+	age_error = models.CharField(max_length=50, blank=True)
+	age_notes = models.TextField(blank=True)
+	features = models.TextField(blank=True)
 
 	class Meta:
 		ordering = ["landslide"]
